eb0e3bbbef4edb7299f86e010872bf556eafcb5a
yassineboujrada <yassine.boujrada@gmail.com>
2022-01-01 04:28:30 +0100

    final d projet

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,redirect,session,request
 from flask.helpers import flash, url_for
 from flask_pymongo import PyMongo
 from data import *  
+import bson
 
 app = Flask(__name__)
 app.secret_key = "maked by yassine boujrada"
@@ -19,17 +20,76 @@ def login():
             flash("votre email ou mot de pass invalid","problem")
             return redirect(url_for("login"))
         else :
-            print("pass test")
+            session["cin"]=admin["_id"]
             return redirect(url_for("home"))
-            # session["prenom"] = admin["prenom"]
-            # session["nom"] = admin["nom"]
-            # session["img"] = admin["img"]
-            # return redirect('/ourStock')
+        
     return render_template('login.html')
 
 @app.route("/home", methods =["GET","POST"])
 def home():
-    return render_template("home.html")
+    co=myData.db.clients.count_documents({})
+    price=myData.db.product.find({"prix":{"$exists":1}},{"_id":0,"prix":1})
+    total=calc_total(price)
+    total_product=myData.db.product.count_documents({})
+    save_courbe(myData)
+    return render_template("home.html",cont_product=co,total=total,product=total_product)
+
+@app.route("/client_information",methods=["POST","GET"])
+def client():
+    client_info=myData.db.clients.find({})
+    return render_template("client.html",table_info=client_info)
+
+@app.route("/settings",methods=["POST","GET"])
+def setting():
+    data=myData.db.admin.find_one({"_id":session["cin"]})
+    adr=data["adresse"]["city"]+" "+data["adresse"]["street"]
+    if request.method=="POST":
+        f=request.files["img_file"]
+        addpict(f,app)
+        
+        if data["full_name"] != request.form.get("user_name"):
+            modify(myData,session["full_name"],"full_name",request.form.get("user_name"))
+        else:
+            if data["age"] != request.form.get("age"):
+                modify(myData,session["cin"],"age",request.form.get("age"))
+            else:
+                # if data["password"] == request.form.get("password"):
+                #     modify(myData,session["cin"],"password",request.form.get("password"))
+                # else:
+                    if data["img"] != f.filename:
+                        modify(myData,session["cin"],"img",f.filename)
+        return redirect(url_for("setting"))
+    else:
+       return render_template("Setting.html",data=data)
+   
+@app.route("/product",methods=["POST","GET"])
+def product():
+    prod=myData.db.product.find({})
+    return render_template("product.html",info_product=prod)
+
+@app.route("/product/add_product",methods=["POST","GET"])
+def add_product():
+    if request.method=="POST":
+        try:
+            x,y=int(request.form.get("Qte")),int(request.form.get("prix"))           
+            if isinstance(x, int) and isinstance(y, int):
+                print(myData.db.produddct.count_documents({})+1,request.form.get("categorie"),request.form.get("nomProduit"),request.form.get("Qte"),request.form.get("prix"),request.form.get("description"))
+               # myData.db.produit.insert_one({"_id":id,"categorie":str(cat),"nomProduit":str(name),"Qte":qte,"prix":prix,"description":str(desc)})
+                insertdb(myData,myData.db.product.count_documents({})+1,request.form.get("categorie"),request.form.get("nomProduit"),request.form.get("Qte"),request.form.get("prix"),request.form.get("description"))
+                flash("you're commande added","success")
+                return redirect(url_for('add_product'))
+        except ValueError:
+            flash("you entere incorrect informations","problem")
+            return redirect(url_for("add_product"))
+    return render_template("add.html")
+
+@app.route("/product/drop_product",methods=["POST","GET"])
+def drop_product():
+    if request.method=="POST":
+        dropprod(myData,request.form.get("categori"),request.form.get("product"))
+        flash("you're commande droped","success")
+        return redirect(url_for('drop_product'))
+    return render_template("drop.html")
 
 
 if __name__=="__main__":
--- data.py
+++ data.py
@@ -1,5 +1,56 @@
+import os
+from flask import Blueprint
+import matplotlib.pyplot as plt
 
 def isEmpty(data):
     if data:
         return False
     return True
+
+def calc_total(t):
+    sum=0
+    for i in t:
+        sum+=i["prix"]
+    return sum
+    
+def modify(db,id,champ,value):
+    db.db.admin.update_one({"_id":int(id)},{"$set":{str(champ):value}})
+    
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def addpict(f,app):
+    UPLOAD_FOLDER = '../projet_mongodb/static/pictures/pofile/'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    auth_print = Blueprint('auth_print', __name__)
+    path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+    print(allowed_file(f.filename))
+    print(f.filename)
+    if allowed_file(f.filename):
+        print("mmmm")
+        f.save(path)
+
+    
+def insertdb(db,id,cat,name,qte,prix,desc):
+    db.db.product.insert_one({"_id":id,"categorie":str(cat),"nomProduit":str(name),"Qte":qte,"prix":prix,"description":str(desc)})
+
+def dropprod(db,name,cat):
+    db.db.product.delete_one({"categorie":str(cat),"nomProduit":str(name)})
+
+
+def save_courbe(db):
+    #plt.figure(figsize=(11,5))
+    x=db.db.product.aggregate([{"$group":{"_id":"$categorie","count": { "$count": { } } }}])
+    for i in x:
+        plt.bar(i["_id"],i["count"])
+
+    # plt.figure(figsize=(8,2))
+    plt.title("statistiques of products")
+    plt.xlabel('Categoties')
+    plt.ylabel('number of categories in stocke')
+    plt.legend()
+    # plt.show()
+    plt.savefig("../projet_mongodb/static/diagramme.png")
+    plt.close()
